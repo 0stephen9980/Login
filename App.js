b854572fc94b0a632e3bc8dabe88282f8736b405
0stephen9980 <prapulchandra1997@gmail.com>
2020-07-03 11:18:23 +0530

    Update

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { container, Button, Input  } 'react-base';
 
 export default function App() {
-  return (    
+  return (      
     <View style={styles.container}>
       <Text>Now here isnt it......</Text>   
       <StatusBar style="auto" />
